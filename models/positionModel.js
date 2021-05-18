ea8fc4135de468923b31486a509c6dc5c1e873c0
BrunoMarini <bgmarini@hotmail.com>
2021-05-18 10:16:48 -0300

    Gathering similar reports

---
+++ models/positionModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const PositionSchema = mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['Depredation', 'Road', 'Leak', 'Garbage', 'Flood'],
+        required: true
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    reports: {
+        type: [String],
+        required: true
+    }
+});
+
+PositionSchema.index({ location: "2dsphere" });
+
+module.exports = mongoose.model('Positions', PositionSchema);
--- models/reportModel.js
+++ models/reportModel.js
@@ -1,27 +1,19 @@
 const mongoose = require('mongoose');
 
 const ReportSchema = mongoose.Schema({
-    type: {
-        type: String,
-        enum: ['Depredation', 'Road', 'Leak', 'Garbage', 'Flood'],
-        required: true
-    },
     description: {
         type: String,
         maxLength: 500
     },
-    position: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-        },
-        coordinates: {
-            type: [Number],
-            required: true
-        }
-    },
     image: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    reportId: {
         type: String,
         required: true
     }
