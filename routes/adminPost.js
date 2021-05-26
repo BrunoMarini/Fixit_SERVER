36a1d619cf9dd7ea7457b46f13091ea402d09ac1
BrunoMarini <bgmarini@hotmail.com>
2021-05-26 09:56:42 -0300

    Adding delete report

--- routes/adminPost.js
+++ routes/adminPost.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const AdminModel = require('../models/adminModel');
+const ReportModel = require('../models/reportModel');
+const PositionModel = require('../models/positionModel');
 const Constants = require('../util/Constants');
 const Utils = require('../util/Utils');
 const TokenGenerator = require ('uuid-token-generator');
@@ -39,10 +41,46 @@ router.post("/login", async (req, res) => {
     }
 });
 
+//TODO move to blocked array and user black list
 router.post("/deleteReport", async (req, res) => {
     console.log("[Server] Delete report");
-    console.log("Text: " + req.body.text);
-    console.log("Id: " + req.body.id);
+    const admin = await Utils.isAdminValid(req);
+    if(!admin) {
+        return res.status(Constants.HTTP_UNAUTHORIZED).json(Utils.createJson(Constants.MESSAGE_NOT_AUTHORIZED));
+    }
+    const report = await ReportModel.findOneAndDelete({ reportId: req.body.id });
+    if(!report) {
+        return res.status(Constants.HTTP_NOT_FOUNT).json(Utils.createJson(Constants.MESSAGE_NOT_FOUND));
+    }
+
+    const position = await PositionModel.findOne({ reports: report.reportId });
+    if(position) {
+        var r = position.reports;
+        if(r.length > 1) {
+            const index = r.indexOf(report.reportId);
+            if(index > -1) {
+                r.splice(index, 1);
+            }
+            position.reports = r;
+            const saved = await position.save();
+            if(!saved) {
+                console.log("[Server] Error saving updated position");
+                //Save report again
+            }
+        } else {
+            PositionModel.deleteOne({ reports: report.reportId }, function(err) {
+                if(err) console.log(err);
+                console.log("[Server] Position deleted successfully!");
+            });
+        }
+    } else {
+        //save report again
+    }
+
+    //GET POSITION AND DELETE THIS REPORT FROM ARRAY
+    if(req.body.blockUser) {
+        // GET USER DELTE HIS REPORTS AND UPDATE ALL
+    }
     return res.status(Constants.HTTP_OK).json(Utils.createJson(Constants.MESSAGE_SUCCESS));
 });
 
