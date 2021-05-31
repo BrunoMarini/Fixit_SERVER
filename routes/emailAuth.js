340e017830fe185fff281eb2628e51f909078275
BrunoMarini <bgmarini@hotmail.com>
2021-05-31 13:39:48 -0300

    Confirmation email enhancement

--- routes/emailAuth.js
+++ routes/emailAuth.js
@@ -16,24 +16,54 @@ module.exports.sendConfirmationEmail  = function(name, email, confirmationCode) 
     });
 
     // Set localhost or server for test purpose
-    var link = '';
+    var link = '', website = '';
     if(process.env.NODE_ENV == 'prod') {
         link = "https://fixit-city.herokuapp.com/confirm/" + confirmationCode;
+        website = "https://fixit-city.herokuapp.com/";
     } else {
         link = "http://localhost:3030/confirm/" + confirmationCode;
+        website = "http://localhost:3030/";
     }
+
+    const path = require('path');
+    const filePath = path.join(__dirname + '/../public/img/logo.png');
 
     // Sending the email
     transport.sendMail({
         from: name,
         to: email,
-        subject: "Please confirm your account",
-        html: `<h1>Email Confirmation</h1>
-                <h2>Hello ${name}</h2>
-                <p>You are one step from helping your city</p>
-                <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
-                <a href=${link}> Click here </a>
+        subject: "FixIt: Confirmação de conta",
+        html:   `<style>
+                .separation {
+                    border-top:5px;
+                    border-bottom: 0px;
+                    border-left: 0px;
+                    border-right: 0px;
+                    border-style: groove;
+                }
+                .btn {
+                    height: 50px;
+                    width: 200px;
+                    background-color: #6340ff;
+                }
+                </style>
+                <center><img src="cid:unique@kreata.ee"/></center>
+                <h1>Confirmação de Email</h1>
+                <h2>Olá, ${name}!</h2>
+                <p>Você está a apenas um passo de conseguir ajudar sua cidade!</p>
+                <p>Obrigado por se registrar no FixIt! Por favor, confirme seu enderço de email clicando no link a baixo!</p>
+                <a href=${link}> CLIQUE AQUI </a>
+                <br>
+                <p> Caso queira saber como suas denúncias serão visualizadas de uma olhada no nosso <a href=${website}> WEBSITE </a></p>
+                <p class="separation"></p>
+                <p>Você está recebendo esse email por ter se registrou no FixIt. Caso esse email tenha sido enviado
+                para você por engano, por favor ignore.</p>
                 </div>`,
+        attachments: [{
+            filename: 'logo.png',
+            path: filePath,
+            cid: 'unique@kreata.ee' //same cid value as in the html img src
+        }]
     }).catch(err => console.log(err));
 };
 
