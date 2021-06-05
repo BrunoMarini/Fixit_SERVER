93e56fefb8410899c0631cfb6f50f147b2f115de
BrunoMarini <bgmarini@hotmail.com>
2021-06-05 07:47:01 -0300

    Saving resolved positions

---
+++ models/resolvedPositionModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const ResolvedPositionSchema = mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['Depredation', 'Road', 'Leak', 'Garbage', 'Flood'],
+        required: true
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    reports: {
+        type: Number,
+        required: true
+    }
+});
+
+ResolvedPositionSchema.index({ location: "2dsphere"});
+ResolvedPositionSchema.index({ coordinates: "2dsphere"});
+
+module.exports = mongoose.model('ResolvedPosition', ResolvedPositionSchema);
